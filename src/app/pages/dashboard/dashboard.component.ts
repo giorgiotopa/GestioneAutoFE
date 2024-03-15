8744dd044e38ba1a302c14e9ccd9c437180b5541
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2024-03-15 12:39:04 +0100

    recupero dettagli utente nella dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
+import { iAccessData } from './../Models/i-access-data';
 import { Component } from '@angular/core';
+import { AuthService } from '../auth/auth.service';
+import { iUser } from '../Models/i-user';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,5 +10,20 @@ import { Component } from '@angular/core';
 })
 export class DashboardComponent {
 
+  userData: iAccessData | null = null;
+  utente!: iUser;
 
+  constructor(private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.authService.user$.subscribe(user => {
+      if (user) {
+        console.log(user);
+        this.utente = user.response;
+        console.log(this.utente);
+      } else {
+        // Gestione nel caso in cui l'utente sia null
+      }
+    });
+  }
 }
