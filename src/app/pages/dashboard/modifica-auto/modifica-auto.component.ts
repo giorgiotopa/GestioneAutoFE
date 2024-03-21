8952d42136182274e68737fe25258e47a9283a1d
giorgiotopa <145937688+giorgiotopa@users.noreply.github.com>
2024-03-21 12:52:27 +0100

    Riadattamento Grafico Pagine Crea/Modifica Auto

--- src/app/pages/dashboard/modifica-auto/modifica-auto.component.ts
+++ src/app/pages/dashboard/modifica-auto/modifica-auto.component.ts
@@ -4,6 +4,7 @@ import { iAuto } from '../../Models/i-auto';
 import { ActivatedRoute } from '@angular/router';
 import { iApiResponseObj } from '../../Models/i-api-response-obj';
 import { iRegisterAuto } from '../../Models/i-register-auto';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-modifica-auto',
